c3400c8dfbefcf34fc029d25dc3ee130fd505818
Gregoire Schiller <gregoire.schiller@gmail.com>
2016-09-28 18:18:04 +0200

    Question model created and first routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#contact'
+  get 'contact', to: 'pages#about'
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
